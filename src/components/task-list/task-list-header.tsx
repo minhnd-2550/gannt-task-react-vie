45abe463db924846ffc313b17297d89861d65f7e
minhnd-2550 <nguyen.duc.minh@sun-asterisk.com>
2024-05-28 21:11:24 +0700

    change name of project

--- src/components/task-list/task-list-header.tsx
+++ src/components/task-list/task-list-header.tsx
@@ -27,7 +27,7 @@ export const TaskListHeaderDefault: React.FC<{
             minWidth: rowWidth,
           }}
         >
-          &nbsp;Name
+          Dự án
         </div>
         <div
           className={styles.ganttTable_HeaderSeparator}
@@ -42,7 +42,7 @@ export const TaskListHeaderDefault: React.FC<{
             minWidth: rowWidth,
           }}
         >
-          &nbsp;From
+          Tham gia từ
         </div>
         <div
           className={styles.ganttTable_HeaderSeparator}
@@ -57,7 +57,7 @@ export const TaskListHeaderDefault: React.FC<{
             minWidth: rowWidth,
           }}
         >
-          &nbsp;To
+          Ngày rời
         </div>
       </div>
     </div>
